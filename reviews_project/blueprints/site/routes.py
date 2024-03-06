43f27db7e883f837d49258c95a49af6b264de857
RazvanBuzincu <razvanbuzincu@gmail.com>
2024-03-06 09:29:13 -0600

    "update and delete left"

--- reviews_project/blueprints/site/routes.py
+++ reviews_project/blueprints/site/routes.py
@@ -1,16 +1,48 @@
 from flask import Blueprint, flash, redirect, render_template, request
 from reviews_project.models import MovieReview, db 
 from reviews_project.forms import MovieReviewForm  
-
+from reviews_project.helpers import get_movie_details
 
 site = Blueprint('site', __name__, template_folder='site_templates')
 
+# @site.route('/')
+# def home():
+#     # Query your database to grab all movie reviews to display
+#     all_reviews = MovieReview.query.all()  
+
+#     return render_template('front.html', reviews=all_reviews)  
+
+# @site.route('/')
+# def home():
+#     all_reviews = MovieReview.query.all()
+    
+#     # Preluăm detalii despre fiecare film folosind titlul din recenzie
+#     for review in all_reviews:
+#         movie_details = get_movie_details(review.movie_title)
+#         # Atașăm detalii la fiecare obiect de recenzie sub forma unui dicționar nou
+#         review.movie_details = movie_details if movie_details != "Movie not found." else None
+
+#     return render_template('front.html', reviews=all_reviews)
+
 @site.route('/')
 def home():
-    # Query your database to grab all movie reviews to display
-    all_reviews = MovieReview.query.all()  
+    all_reviews = MovieReview.query.all()
+    reviews_with_details = []
 
-    return render_template('front.html', reviews=all_reviews)  
+    for review in all_reviews:
+        movie_details = get_movie_details(review.movie_title)
+        # Verifică dacă filmul a fost găsit înainte de a adăuga detalii
+        if movie_details != "Movie not found.":
+            review_data = {
+                'review': review,
+                'movie_details': movie_details
+            }
+            reviews_with_details.append(review_data)
+        else:
+            reviews_with_details.append({'review': review})
+
+    return render_template('front.html', reviews=reviews_with_details)
+
 
 
 @site.route('/create', methods=['GET', 'POST'])
@@ -30,21 +62,21 @@ def create_review():
     
     return render_template('reviewform.html', form=form)
 
-@site.route('/update/<int:review_id>', methods=['GET', 'POST'])
+@site.route('/update/<review_id>', methods=['GET', 'POST'])
 def update_review(review_id):
     review = MovieReview.query.get_or_404(review_id)
     form = MovieReviewForm(obj=review)
     if form.validate_on_submit():
         review.movie_title = form.movie_title.data
         review.review_text = form.review_text.data
-        review.rating = form.rating.data
+        review.rating = float(form.rating.data)
         review.reviewer_name = form.reviewer_name.data
         db.session.commit()
         flash('Review updated successfully!', 'success')
         return redirect('/')
-    return render_template('update.html', form=form)
+    return render_template('update_review.html', form=form, movie_review=review)
 
-@site.route('/delete/<int:review_id>')
+@site.route('/delete/<review_id>')
 def delete_review(review_id):
     review = MovieReview.query.get_or_404(review_id)
     db.session.delete(review)
